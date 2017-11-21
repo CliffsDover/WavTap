d00392d65fdb28512cb92efcead5f4935d8c32ed
Mayu Laierlence <minacle@live.com>
2017-11-22 07:27:45 +0900

    Set install destination to /Library/Extensions

--- Installer/kext_postinstall.sh
+++ Installer/kext_postinstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PRODUCT_NAME=WavTap
-INSTALL_DESTINATION=/System/Library/Extensions
+INSTALL_DESTINATION=/Library/Extensions
 
 sudo chmod -R 700 "$INSTALL_DESTINATION/$PRODUCT_NAME.kext"
 sudo chown -R root:wheel "$INSTALL_DESTINATION/$PRODUCT_NAME.kext"
--- Installer/kext_preinstall.sh
+++ Installer/kext_preinstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PRODUCT_NAME=WavTap
-INSTALL_DESTINATION=/System/Library/Extensions
+INSTALL_DESTINATION=/Library/Extensions
 
 # if [[ "$(kextstat | grep $PRODUCT_NAME | grep -v grep)" ]]; then sudo kextunload "$INSTALL_DESTINATION/$PRODUCT_NAME.kext"; fi
 sudo rm -rf "$INSTALL_DESTINATION/$PRODUCT_NAME.kext"
